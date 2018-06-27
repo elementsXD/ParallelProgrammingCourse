830f59cb055a6db0bbc5188e612d6f4904cfbfc5
Erik Silva Nascimento <erik.silva_10@hotmail.com>
2018-06-26 22:07:53 -0300

    Aula 5 sobre esperar até determinado tempo para cancelar a tarefa, caso não seja inserido nenhum comando em até 5 segundos ela não é cancelada.

---
+++ Class5PPWaitingForTimeToPass/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Class5PPWaitingForTimeToPass
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            var cts = new CancellationTokenSource();
+            var token = cts.Token;
+            var t = new Task(() =>
+            {
+                Console.WriteLine("Press any key to disarm; you have 5 seconds!!!");
+                bool canceled = token.WaitHandle.WaitOne(5000);
+                Console.WriteLine(canceled ? "\nDisarmed" : "\nBoom!!!! ");
+
+            }, token);
+            t.Start();
+            
+            Console.ReadKey();
+            cts.Cancel();
+
+
+            Console.ReadKey();
+            Console.WriteLine("\nMain program done.");
+            Console.ReadKey();
+        }
+    }
+}
